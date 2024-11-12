12b398eedd514b0f82c5566224525e9790b1f44e
Simon Campbell <sjcampbell750@gmail.com>
2024-11-12 21:49:00 +0000

    feat: ball

---
+++ Assets/Scripts/Entities/Ball.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb2d;
+
+    [SerializeField] private float initialForce;
+
+    [SerializeField] private float speed = 10f;
+
+    private Vector2 initialPosition;
+    
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        EventController.GameStart += GameStart;
+        EventController.LifeEnd += ResetBall;
+        initialPosition = transform.position;
+    }
+
+    private void OnDestroy()
+    {
+        EventController.GameStart -= GameStart;
+        EventController.LifeEnd -= ResetBall;
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.linearVelocity = rb2d.linearVelocity.normalized * speed;
+    }
+
+    private void GameStart()
+    {
+        float angle = Random.Range(-10, 10);
+        var randomUpwardsVelocty = new Vector2(angle, 1);
+        rb2d.AddForce(randomUpwardsVelocty * initialForce, ForceMode2D.Impulse);
+    }
+
+    private void ResetBall()
+    {
+        rb2d.linearVelocity = Vector2.zero;
+        transform.position = initialPosition;
+    }
+}
