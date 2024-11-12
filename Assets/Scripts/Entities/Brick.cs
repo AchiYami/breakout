593d19ea6846887989d5f953c6b26c9a44f48838
Simon Campbell <sjcampbell750@gmail.com>
2024-11-12 21:49:12 +0000

    feat: brick

---
+++ Assets/Scripts/Entities/Brick.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.transform.CompareTag("Ball"))
+        {
+            Destroy(gameObject);
+            EventController.OnBrickDestroyed?.Invoke();
+        }
+    }
+}
