c260580db2560d784009770b00b10ea6fe1cf890
Simon Campbell <sjcampbell750@gmail.com>
2024-11-25 17:31:41 +0000

    feat: brick destroyed vfx

--- Assets/Scripts/Entities/Brick.cs
+++ Assets/Scripts/Entities/Brick.cs
@@ -1,20 +1,27 @@
+using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
+    [SerializeField] [FoldoutGroup("Effects")]
+    private GameObject onDestroyedPrefab;
 
     private Level _level;
 
-    public void Initialize(Level level)
-    {
-        
-    }
-    
     private void OnCollisionEnter2D(Collision2D other)
     {
-
         if (other.transform.CompareTag("Ball"))
         {
+            var vfx = Instantiate(onDestroyedPrefab, transform.position, Quaternion.identity);
+
+            if (vfx.TryGetComponent<ParticleSystem>(out var onDestroyedPrefabParticles))
+            {
+                var mainModule = onDestroyedPrefabParticles.main;
+                mainModule.startColor = GetComponent<SpriteRenderer>().color;
+            }
+
+
             gameObject.SetActive(false);
             EventController.OnBrickDestroyed?.Invoke();
         }
